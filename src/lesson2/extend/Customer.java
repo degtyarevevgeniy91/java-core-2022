170aacff113390d7f162f53427de01dec5e32a92
degtyarev.evgeniy91 <degtyarev.evgeniy91@gmail.com>
2022-11-20 14:09:43 +0200

    -

---
+++ src/lesson2/extend/Customer.java
@@ -0,0 +1,16 @@
+package lesson2.extend;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+}
